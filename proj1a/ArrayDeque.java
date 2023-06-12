3b2d87b3c3cbdd48cb44addcf037557a7bdf906c
政宇 廖 <vince.liao@nextbank.com.tw>
2023-06-12 15:36:02 +0800

    fix parts failed the test

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -1,39 +1,20 @@
-interface Deque <T>{
-    void addFirst(T item); // Adds an item of type T to the front of the deque.
-    void addLast(T item); // Adds an item of type T to the back of the deque.
-    boolean isEmpty(); // Returns true if deque is empty, false otherwise.
-    int size(); //Returns the number of items in the deque.
-    void printDeque(); //Prints the items in the deque from first to last, separated by a space. Once all the items have been printed, print out a new line.
-    T removeFirst(); // Removes and returns the item at the front of the deque. If no such item exists, returns null.
-    T removeLast(); // Removes and returns the item at the back of the deque. If no such item exists, returns null.
-    T get(int index); // Gets the item at the given index, where 0 is the front, 1 is the next item, and so forth. If no such item exists, returns null. Must not alter the deque!
-}
-
-
-public class ArrayDeque<T> implements Deque<T> {
+public class ArrayDeque<T> {
     private int arraySize = 8;
     private int size = 0;
     private T[] arr;
 
-    static final double USAGE_RATIO = 0.25;
-    static final double LOW_USAGE_RATIO = 0.05;
+    private static final double USAGE_RATIO = 0.25;
 
     public ArrayDeque() {
         this.arr = (T[]) new Object[this.arraySize];
     }
 
-    public ArrayDeque(ArrayDeque<T> other) {
-        this.arr = copyArray(other);
-    }
-
-    @Override
     public int size() {
         return this.size;
     }
 
-    @Override
     public void addFirst(T item) {
-        if(this.size > 1) {
+        if (this.size >= 1) {
             if (needResize()) {
                 increaseArraySize();
             }
@@ -44,10 +25,9 @@ public class ArrayDeque<T> implements Deque<T> {
         } else {
             this.arr[0] = item;
         }
-        this.size+=1;
+        this.size += 1;
     }
 
-    @Override
     public void addLast(T item) {
         if (needResize()) {
             increaseArraySize();
@@ -56,24 +36,22 @@ public class ArrayDeque<T> implements Deque<T> {
             this.arr = newArray;
         }
         this.arr[this.size] = item;
-        this.size+=1;
+        this.size += 1;
     }
 
-    @Override
     public boolean isEmpty() {
         return size == 0;
     }
 
-    @Override
     public void printDeque() {
-        for(int i = 0; i < this.size; i++) {
-            System.out.println(arr[i]);
+        for (int i = 0; i < this.size; i++) {
+            System.out.print(arr[i]);
+            System.out.println(" ");
         }
     }
 
-    @Override
     public T removeFirst() {
-        if(needReduceSize()) {
+        if (needReduceSize()) {
             reduceArraySize();
         }
         T[] newArray = (T[]) new Object[this.arraySize];
@@ -84,12 +62,11 @@ public class ArrayDeque<T> implements Deque<T> {
         return firstItem;
     }
 
-    @Override
     public T removeLast() {
         T lastItem = this.arr[this.size - 1];
         this.arr[this.size - 1] = null;
         this.size -= 1;
-        if(needReduceSize()) {
+        if (needReduceSize()) {
             reduceArraySize();
             T[] newArray = (T[]) new Object[this.arraySize];
             System.arraycopy(this.arr, 0, newArray, 0, this.size);
@@ -98,7 +75,6 @@ public class ArrayDeque<T> implements Deque<T> {
         return lastItem;
     }
 
-    @Override
     public T get(int index) {
         return this.arr[index];
     }
@@ -114,12 +90,11 @@ public class ArrayDeque<T> implements Deque<T> {
     }
 
     private boolean needReduceSize() {
-        double ratio = this.size >= 16 ? USAGE_RATIO : LOW_USAGE_RATIO;
-        return ((double) (this.size - 1) / this.arraySize) < ratio;
+        return this.arraySize >= 16 && (double) (this.size - 1) / this.arraySize < USAGE_RATIO;
     }
 
     private void increaseArraySize() {
-        if(this.arraySize < 16) {
+        if (this.arraySize < 16) {
             this.arraySize = this.arraySize * 2;
         } else {
             this.arraySize = (int) Math.round(this.arraySize / USAGE_RATIO);
@@ -127,7 +102,6 @@ public class ArrayDeque<T> implements Deque<T> {
     }
 
     private void reduceArraySize() {
-        double ratio = this.size >= 16 ? USAGE_RATIO : LOW_USAGE_RATIO;
-        this.arraySize = (int) Math.round(this.size / (1.0 - ratio));
+        this.arraySize = (int) Math.round(this.size / (1.0 - USAGE_RATIO));
     }
 }
--- proj1a/LinkedListDeque.java
+++ proj1a/LinkedListDeque.java
@@ -1,32 +1,23 @@
-interface Deque <T>{
-    void addFirst(T item); // Adds an item of type T to the front of the deque.
-    void addLast(T item); // Adds an item of type T to the back of the deque.
-    boolean isEmpty(); // Returns true if deque is empty, false otherwise.
-    int size(); //Returns the number of items in the deque.
-    void printDeque(); //Prints the items in the deque from first to last, separated by a space. Once all the items have been printed, print out a new line.
-    T removeFirst(); // Removes and returns the item at the front of the deque. If no such item exists, returns null.
-    T removeLast(); // Removes and returns the item at the back of the deque. If no such item exists, returns null.
-    T get(int index); // Gets the item at the given index, where 0 is the front, 1 is the next item, and so forth. If no such item exists, returns null. Must not alter the deque!
-}
+public class LinkedListDeque<T> {
+    static class Node<T> {
+        Node<T> prev;
+        Node<T> next;
+        T value;
 
-class Node<T> {
-    Node<T> prev;
-    Node<T> next;
-    T value;
-    public Node(T value) {
-        this.value = value;
-        this.next = null;
-        this.prev = null;
+        public Node(T value) {
+            this.value = value;
+            this.next = null;
+            this.prev = null;
+        }
+
+        public Node() {
+            this.value = null;
+            this.next = null;
+            this.prev = null;
+        }
     }
 
-    public Node() {
-        this.value = null;
-        this.next = null;
-        this.prev = null;
-    }
-}
 
-public class LinkedListDeque<T> implements Deque<T> {
     private final Node<T> sentinel;
     private int size;
 
@@ -34,13 +25,12 @@ public class LinkedListDeque<T> implements Deque<T> {
         this.sentinel = new Node<>();
     }
 
-    @Override
     public void addFirst(T item) {
         Node<T> originalNextPointer = this.sentinel.next;
-        Node<T> newNode =  new Node<>(item);
+        Node<T> newNode = new Node<>(item);
         this.sentinel.next = newNode;
         newNode.prev = this.sentinel;
-        if(originalNextPointer == null) {
+        if (originalNextPointer == null) {
             this.sentinel.prev = newNode;
             newNode.next = this.sentinel;
         } else {
@@ -50,36 +40,32 @@ public class LinkedListDeque<T> implements Deque<T> {
         this.size++;
     }
 
-    @Override
     public void addLast(T item) {
-       Node<T> newNode = new Node<>(item);
-       Node<T> originalLastNode = this.sentinel.prev;
-       this.sentinel.prev = newNode;
+        Node<T> newNode = new Node<>(item);
+        Node<T> originalLastNode = this.sentinel.prev;
+        this.sentinel.prev = newNode;
         newNode.next = this.sentinel;
-       if (originalLastNode == null) {
-          this.sentinel.next = newNode;
-          newNode.prev = this.sentinel;
-       } else {
-           originalLastNode.next = newNode;
-           newNode.prev = originalLastNode;
-       }
-       this.size++;
+        if (originalLastNode == null) {
+            this.sentinel.next = newNode;
+            newNode.prev = this.sentinel;
+        } else {
+            originalLastNode.next = newNode;
+            newNode.prev = originalLastNode;
+        }
+        this.size++;
     }
 
-    @Override
     public boolean isEmpty() {
         return this.size == 0;
     }
 
-    @Override
     public int size() {
         return this.size;
     }
 
-    @Override
     public void printDeque() {
         Node<T> node = this.sentinel.next;
-        while (node != null && node != this.sentinel){
+        while (node != null && node != this.sentinel) {
             System.out.print(node.value);
             System.out.print(" ");
             node = node.next;
@@ -87,13 +73,12 @@ public class LinkedListDeque<T> implements Deque<T> {
         System.out.println();
     }
 
-    @Override
     public T removeFirst() {
         Node<T> firstNode = this.sentinel.next;
-        if(firstNode == null) {
+        if (firstNode == null) {
             return null;
         }
-        if(this.size == 1) {
+        if (this.size == 1) {
             this.sentinel.next = null;
             this.sentinel.prev = null;
         } else {
@@ -104,13 +89,12 @@ public class LinkedListDeque<T> implements Deque<T> {
         return firstNode.value;
     }
 
-    @Override
     public T removeLast() {
         Node<T> originalLastNode = this.sentinel.prev;
-        if(originalLastNode == null) {
+        if (originalLastNode == null) {
             return null;
         }
-        if(this.size == 1) {
+        if (this.size == 1) {
             this.sentinel.prev = null;
             this.sentinel.next = null;
         } else {
@@ -121,16 +105,15 @@ public class LinkedListDeque<T> implements Deque<T> {
         return originalLastNode.value;
     }
 
-    @Override
     public T get(int index) {
-        if(index >= size) {
+        if (index >= size) {
             return null;
         }
         Node<T> targetNode;
         do {
             targetNode = this.sentinel.next;
             index--;
-        }while(index != -1);
+        } while (index != -1);
         return targetNode.value;
     }
 }
