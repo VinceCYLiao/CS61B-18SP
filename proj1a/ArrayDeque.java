728474e1d9e73cab1688153ad77b2f4836d45d05
政宇 廖 <vince.liao@nextbank.com.tw>
2023-06-13 14:29:33 +0800

    feat: update to circular sentinel approach

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -2,6 +2,8 @@ public class ArrayDeque<T> {
     private int arraySize = 8;
     private int size = 0;
     private T[] arr;
+    private int first;
+    private int last;
 
     private static final double USAGE_RATIO = 0.25;
 
@@ -13,30 +15,66 @@ public class ArrayDeque<T> {
         return this.size;
     }
 
-    public void addFirst(T item) {
-        if (this.size >= 1) {
-            if (needResize()) {
-                increaseArraySize();
+    private void resize(int arraySize) {
+        this.arraySize = arraySize;
+        T[] newArray = (T[]) new Object[this.arraySize];
+        // handle resize when array is full
+        if(this.size == this.arr.length) {
+            System.arraycopy(this.arr, this.first, newArray, 0, this.size - this.first);
+            if (this.first != 0) {
+                System.arraycopy(this.arr, 0, newArray, this.size - this.first, this.first);
             }
-            T[] newArray = (T[]) new Object[this.arraySize];
-            newArray[0] = item;
-            System.arraycopy(this.arr, 0, newArray, 1, this.size);
-            this.arr = newArray;
+
         } else {
-            this.arr[0] = item;
+            // handle array length > 16 & usage ratio < 25%;
+            if (this.first < this.last) {
+                System.arraycopy(this.arr, this.first, newArray, 0, this.size);
+            } else {
+                System.arraycopy(this.arr, this.first, newArray, 0, this.arr.length);
+                System.arraycopy(this.arr, 0, newArray, this.arr.length, this.last + 1);
+            }
         }
+        this.arr = newArray;
+        this.first = 0;
+        this.last = this.size - 1;
+    }
+
+    private boolean shouldReduceArraySize() {
+        return this.arraySize > 16 && ((double) this.size / this.arraySize) < USAGE_RATIO;
+    }
+
+    private void recalculateFirst() {
+        int nextFirst = this.first - 1;
+        this.first = nextFirst < 0 ? this.arraySize - 1 : nextFirst;
+    }
+
+    private void recalculateLast() {
+        int nextLast = this.last + 1;
+        this.last = nextLast == this.arraySize ? 0 : nextLast;
+    }
+
+    public void addFirst(T item) {
+        if (this.size == this.arraySize) {
+            resize(this.arraySize * 2);
+        }
+        if(this.size >= 1) {
+            recalculateFirst();
+        }
+        this.arr[first] = item;
         this.size += 1;
+
     }
 
     public void addLast(T item) {
-        if (needResize()) {
-            increaseArraySize();
-            T[] newArray = (T[]) new Object[this.arraySize];
-            System.arraycopy(this.arr, 0, newArray, 0, this.size);
-            this.arr = newArray;
+        if (this.size == this.arraySize) {
+            resize(this.arraySize * 2);
         }
-        this.arr[this.size] = item;
+        if(this.size >= 1) {
+            recalculateLast();
+        }
+        this.arr[last] = item;
         this.size += 1;
+
     }
 
     public boolean isEmpty() {
@@ -44,70 +82,63 @@ public class ArrayDeque<T> {
     }
 
     public void printDeque() {
-        for (int i = 0; i < this.size; i++) {
-            System.out.print(arr[i]);
-            System.out.println(" ");
+        if (isEmpty()) {
+            return;
         }
+        if(this.first < this.last) {
+            for (int i = this.first; i <= this.last; i++) {
+                System.out.print(this.arr[i]);
+                System.out.print(" ");
+            }
+        } else {
+            for (int i = this.first; i < this.arr.length; i++) {
+                System.out.print(this.arr[i]);
+                System.out.print(" ");
+            }
+            for (int i = 0; i <= this.last; i++) {
+                System.out.print(this.arr[i]);
+                System.out.print(" ");
+            }
+        }
+        System.out.println();
     }
 
     public T removeFirst() {
         if (this.size == 0) {
             return null;
         }
-        if (needReduceSize()) {
-            reduceArraySize();
-        }
-        T[] newArray = (T[]) new Object[this.arraySize];
-        T firstItem = this.arr[0];
+        T item = this.arr[this.first];
+        this.arr[this.first] = null;
         this.size -= 1;
-        System.arraycopy(this.arr, 1, newArray, 0, this.size);
-        this.arr = newArray;
-        return firstItem;
+        // not only one item in which situation two pointers are the same
+        if (this.first != this.last) {
+            this.first = this.first + 1 == this.arr.length ? 0 : this.first + 1;
+        }
+        if (shouldReduceArraySize()) {
+            resize((int) Math.round(this.size / (1 - USAGE_RATIO)));
+        }
+        return item;
     }
 
     public T removeLast() {
         if (this.size == 0) {
             return null;
         }
-        T lastItem = this.arr[this.size - 1];
-        this.arr[this.size - 1] = null;
+        T item = this.arr[this.last];
         this.size -= 1;
-        if (needReduceSize()) {
-            reduceArraySize();
-            T[] newArray = (T[]) new Object[this.arraySize];
-            System.arraycopy(this.arr, 0, newArray, 0, this.size);
-            this.arr = newArray;
+        if (this.first != this.last) {
+            this.last = this.last - 1 < 0 ? this.arr.length - 1 : this.last - 1;
         }
-        return lastItem;
+        if (shouldReduceArraySize()) {
+            resize((int) Math.round(this.size / (1 - USAGE_RATIO)));
+        }
+        return item;
     }
 
     public T get(int index) {
-        return this.arr[index];
-    }
-
-    private static <T> T[] copyArray(ArrayDeque<T> other) {
-        T[] copyArr = (T[]) new Object[other.size()];
-        System.arraycopy(other.arr, 0, copyArr, 0, other.size());
-        return copyArr;
-    }
-
-    private boolean needResize() {
-        return this.size == this.arraySize;
-    }
-
-    private boolean needReduceSize() {
-        return this.arraySize >= 16 && (double) (this.size - 1) / this.arraySize < USAGE_RATIO;
-    }
-
-    private void increaseArraySize() {
-        if (this.arraySize < 16) {
-            this.arraySize = this.arraySize * 2;
-        } else {
-            this.arraySize = (int) Math.round(this.arraySize / USAGE_RATIO);
+        if(index >= this.arraySize) {
+            return null;
         }
-    }
-
-    private void reduceArraySize() {
-        this.arraySize = (int) Math.round(this.size / (1.0 - USAGE_RATIO));
+        return this.arr[index];
     }
 }
